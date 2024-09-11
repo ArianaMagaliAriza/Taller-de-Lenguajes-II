9598b964450e4eea7ab0637d7588a662fa530c4c
ArianaMagaliAriza <arianamagaliariza@gmail.com>
2024-09-11 00:58:00 -0300

    Update Cripto Stock

--- Workspace/Entregable1/src/billetera/Criptomoneda.java
+++ Workspace/Entregable1/src/billetera/Criptomoneda.java
@@ -4,23 +4,27 @@ package billetera;
  * @nombre nombre es el nombre de la criptomoneda (ej:Bitcoin,Etherium,etc)
  * @etiqueta etiqueta son las siglas que identifican a la criptomoneda (ej:BTC,ETH,etc)
  * @volatilidad volatilidad es el porcentaje de fluctación del precio de la criptomoneda
+ * @stock stock es la cantidad de la criptomoneda que se encuentra disponible en el mercado
  * 
  */
 public class Criptomoneda {
 	private String nombre;
 	private String etiqueta;
 	private Double volatilidad;
+	private Double stock;
 	
-	public Criptomoneda(String nombre, String etiqueta, Double volatilidad) {
+	
+	public Criptomoneda(String nombre, String etiqueta, Double volatilidad, Double stock) {
 		this.nombre = nombre;
 		this.etiqueta = etiqueta;
 		this.volatilidad = volatilidad;
+		this.stock = stock;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
@@ -39,6 +43,14 @@ public class Criptomoneda {
 	
 	public void setVolatilidad(Double volatilidad) {
 		this.volatilidad = volatilidad;
+	}
+	
+	public Double getStock() {
+		return stock;
+	}
+
+	public void setStock(Double stock) {
+		this.stock = stock;
 	}
 
 }
