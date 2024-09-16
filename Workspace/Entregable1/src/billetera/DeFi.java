5d9556c5b74f5e4234abe318296787565a2cc67b
Ariana Magali Ariza <ariana.ariza@alu.ing.unlp.edu.ar>
2024-09-16 20:20:05 -0300

    Add files via upload

---
+++ Workspace/Entregable1/src/billetera/DeFi.java
@@ -0,0 +1,49 @@
+package billetera;
+
+public class DeFi {
+    private Criptomoneda moneda;
+    private String protocolo;
+    private Double monto;
+    private Double interes;
+    
+    /**
+     * @param moneda es el tipo de criptomoneda en el que se realiza el DeFi
+     * @param protocolo es el protocolo del DeFi
+     * @param monto es el monto en cripto depositado en el DeFi
+     * @param interes es el interes generado por el DeFi
+     * 
+     */
+
+    public DeFi(Criptomoneda moneda, String protocolo, Double monto, Double interes) {
+        this.moneda = moneda;
+        this.protocolo = protocolo;
+        this.monto = monto;
+        this.interes = interes;
+    }
+
+    public Criptomoneda getMoneda() {
+        return moneda;
+    }
+    public void setMoneda(Criptomoneda moneda) {
+        this.moneda = moneda;
+    }
+    public String getProtocolo() {
+        return protocolo;
+    }
+    public void setProtocolo(String protocolo) {
+        this.protocolo = protocolo;
+    }
+    public Double getMonto() {
+        return monto;
+    }
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+    public Double getInteres() {
+        return interes;
+    }
+    public void setInteres(Double interes) {
+        this.interes = interes;
+    }
+    
+}
