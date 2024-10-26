1a5be76a86e4531490713bb85917523ffaf19795
Ariana Magali Ariza <arianamagaliariza@gmail.com>
2024-10-26 19:56:38 -0300

    Delete Entregable_2 directory

--- Entregable_2/Workspace/Entregable_2/src/dao/creacionTablas.java
+++
@@ -1,5 +0,0 @@
-package dao;
-
-public class creacionTablas {
-    // HAY QUE HACER LOS INSERT ACÁ
-}
--- Entregable_2/Workspace/Entregable_2/src/modelo/Billetera.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-public class Billetera {
-
-	public Billetera() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
