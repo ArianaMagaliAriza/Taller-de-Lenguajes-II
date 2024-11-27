0e7928cf021d92295db9011f25a66894e183fe25
Ariana Magali Ariza <arianamagaliariza@gmail.com>
2024-11-26 21:45:39 -0300

    Delete Reentrega-TP2 directory

--- Reentrega-TP2/src/modelo/ComparadorNomenclaturaActivo.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-import java.util.Comparator;
-
-public class ComparadorNomenclaturaActivo implements Comparator<Activo>{
-	public int compare(Activo a1,Activo a2) {
-		return a1.getNomenclatura().compareTo(a2.getNomenclatura());
-	}
-}
--- Reentrega-TP2/src/modelo/ComparadorNomenclaturaMoneda.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-import java.util.Comparator;
-
-public class ComparadorNomenclaturaMoneda implements Comparator<Moneda>{
-	public int compare(Moneda m1,Moneda m2) {
-		return m1.getNomenclatura().compareTo(m2.getNomenclatura());
-	}
-}
