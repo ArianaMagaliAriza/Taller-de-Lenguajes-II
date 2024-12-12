80e595a5ea57fa79688f44ba210bf235b7469864
lautarolopes65 <136150572+lautarolopes65@users.noreply.github.com>
2024-12-12 19:19:31 -0300

    Delete Entregable2 directory

--- Entregable2/src/dao/PersonaDAO.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import modelo.Persona;
-
-public interface PersonaDAO {
-	public int cargarPersona(Persona persona);
-}
--- Entregable2/src/dao/UsuarioDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import modelo.Usuario;
-
-public interface UsuarioDAO {
-	public void cargarUsuario(Usuario usuario);
-	public boolean usuarioEnBD(String email);
-	public Usuario obtenerUsuario(String email); 
-}
