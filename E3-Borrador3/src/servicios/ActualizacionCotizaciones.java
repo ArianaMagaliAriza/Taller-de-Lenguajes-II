a03377dd35642bf151a85829fb8db3b238b8d745
lautarolopes65 <136150572+lautarolopes65@users.noreply.github.com>
2024-12-16 19:34:02 -0300

    Update ActualizacionCotizaciones.java

--- E3-Borrador3/src/servicios/ActualizacionCotizaciones.java
+++ E3-Borrador3/src/servicios/ActualizacionCotizaciones.java
@@ -9,13 +9,15 @@ public class ActualizacionCotizaciones {
     private Timer timer;
     private ConsultarPrecioCripto consultarPrecioCripto;
     private CotizacionesGUI cotizacionesGUI;
+    private Controladores controlador;
     			
-    public ActualizacionCotizaciones(CotizacionesGUI cotizacionesGUI) {
+    public ActualizacionCotizaciones(CotizacionesGUI cotizacionesGUI,Controladores controlador) {
         this.cotizacionesGUI = cotizacionesGUI;
+        this.controlador=controlador;
         this.consultarPrecioCripto = new ConsultarPrecioCripto(this);
         this.timer = new Timer();
         
-        // Programar la tarea para ejecutarse cada 5 segundos
+        // Programar la tarea para ejecutarse cada 1 minuto
         timer.schedule(consultarPrecioCripto, 0,60000);
         
     }
@@ -23,7 +25,9 @@ public class ActualizacionCotizaciones {
         timer.cancel(); // Detiene el timer
     }
     public void actualizarPrecios(List<Double>precios) {
+    	// Actualizar cotizaciones en la vista
     	cotizacionesGUI.actualizarCotizaciones(precios.get(0),precios.get(1),precios.get(2),precios.get(3),precios.get(4));
-    	
+    	// Actualizar cotizaciones en la BD
+    	controlador.actualizarCotizaciones(precios);
     }
 }
